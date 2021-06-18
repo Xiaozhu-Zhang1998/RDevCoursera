467a05f5724b6af40fa159ebbb768341be6b0040
Xiaozhu Zhang <xzzhang1998@gmail.com>
2021-06-18 17:51:31 +0800

    Updated

--- Shiny/27_Shinydashboard/server.R
+++ Shiny/27_Shinydashboard/server.R
@@ -1,9 +1,54 @@
 library(shiny)
+library(shinydashboard)
+library(plotly)
+library(datasets)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
     
     output$mydatatable <- renderDataTable({
         mtcars
+    })
+    
+    output$data <- renderTable({
+      head(mtcars)
+    })
+    
+    output$plot1 <- renderPlotly({
+      plot_ly(data = mtcars,
+              x =~ wt,
+              y =~ mpg,
+              type = 'scatter',
+              mode = 'markers')
+    })
+    
+    output$plot2 <- renderPlotly({
+      plot_ly(data = mtcars,
+              x =~ mpg,
+              type =~ "histogram")
+    })
+    
+    output$inputwidget <- renderUI({
+      selectInput("in", "Select a variable", choices = names(mtcars))
+    })
+    
+    output$min_ <- renderInfoBox({
+      infoBox(title = "Minm", value = min(s), subtitle = "minimum value in dataset", fill = TRUE)
+    })
+    
+    output$max_ <- renderInfoBox({
+      infoBox(title = "Maxm", value = max(s), subtitle = "maximum value in dataset", fill = TRUE, color = "yellow")
+    })
+    
+    output$sd_ <- renderInfoBox({
+      infoBox(title = "SD", value = round(sd(s), 2), subtitle = "Standard Deviation", icon = icon("arrow-up"))
+    })
+    
+    output$mean_ <- renderInfoBox({
+      infoBox(title = "Mean", value = mean(s), subtitle = "Mean of dataset values", icon = icon("angle-double-right"))
+    })
+    
+    output$median_ <- renderInfoBox({
+      infoBox(title = tags$b("Median"), value = median(s), subtitle = tags$i("Median of dataset"), icon = icon("angle-double-right"))
     })
 
 })
--- Shiny/27_Shinydashboard/ui.R
+++ Shiny/27_Shinydashboard/ui.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(shinydashboard)
+library(plotly)
+library(datasets)
 
 shinyUI(dashboardPage(
 
@@ -9,14 +11,53 @@ shinyUI(dashboardPage(
         sidebarMenu(
             menuItem(text = "About", tabName = "about", icon = icon("clipboard")),
             menuItem(text = "Data", tabName = "data", icon = icon("database"), badgeLabel = "new", badgeColor = "green"),
-            menuItem(text = "Link to code files", href = "https://www.google.com", icon = icon("code"))
+            menuItem(text = "Link to code files", href = "https://www.google.com", icon = icon("code")),
+            menuItem("Chart", icon = icon("line-chart"), 
+                     menuSubItem('chartmenusub1', tabName = "chart1", icon = icon("line-chart")),
+                     menuSubItem("chartmenushb2", tabName = "chart2", icon = icon("line-chart"))),
+            menuItem(text = "Box", tabName = "box", icon = icon("check")),
+            menuItem(text = "InfoBox", tabName = "IB")
         )
     ),
     
     dashboardBody(
         tabItems(
             tabItem(tabName = "about", p("This example app...")),
-            tabItem(tabName = "data", dataTableOutput("mydatatable"))
+            tabItem(tabName = "data", dataTableOutput("mydatatable")),
+            tabItem(tabName = "box", 
+                    fluidRow(
+                        box(title = "Box with a plot", plotlyOutput("plot1", height = 250), 
+                            status = "danger", solidHeader = T, collapsible = T),
+                        
+                        # Visit status options as follows
+                        # https://rstudio.github.io/shinydashboard/appearance.html#statuses-and-colors
+                        
+                        box(title = "Box with a plot", plotlyOutput("plot2", height = 250))
+                    ),
+                    fluidRow(
+                        box(title = "Box with datatable", tableOutput("data"), width = 8,
+                            solidHeader = T, status = "success", background = "red"),
+                        box(title = "Box with inputwidget", uiOutput("inputwidget"), width = 4,
+                            background = "black", solidHeader = T)
+                    ),
+                    fluidRow(
+                        tabBox(id = "tabchart",
+                               tabPanel("Tab1", "Tab 1 content"),
+                               tabPanel("Tab2", "Tab 2 content"))
+                    )),
+            
+            tabItem(tabName = "IB",
+                    fluidRow(
+                        infoBoxOutput("min_", width = 3), infoBoxOutput("max_", width = 3),
+                        infoBoxOutput("sd_", width = 3), infoBoxOutput("mean_", width = 3)
+                    ),
+                    fluidRow(
+                        infoBoxOutput("median_", width = 6)
+                    ),
+                    fluidRow(
+                        infoBoxOutput("inline", tags$style("#inline {height:75px; line-height:75px; padding-top:0px; padding-bottom:0px; width:400px;}"))
+                    )
+                    )
         )
     )
     
